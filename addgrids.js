5dc66d719ec4510399c14960f04d177ad39b5749
Venka <venkasritharan@gmail.com>
2022-01-14 18:01:33 -0500

    edited html file

--- addgrids.js
+++ addgrids.js
@@ -1,13 +1,13 @@
 const TOTAL_DIVS = 20;
 const mainContainer = document.querySelector('.container');
-const operations = ['C', '( )', '%', '/', 7, 8, 9, 'x', 4, 5, 6,
+const digits = ['C', '( )', '%', '/', 7, 8, 9, 'x', 4, 5, 6,
                         '-', 1, 2, 3, '+', '+/-', 0, '.', '='];
 
 for (let i = 0; i < TOTAL_DIVS; i++) {
     const div = document.createElement('div');
     mainContainer.append(div);
     div.classList.add('box');
-    div.innerHTML = operations[i];
+    div.innerHTML = digits[i];
 }
 
 
